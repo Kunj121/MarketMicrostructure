2e56f619691ffa54b9c5afc741ef750969dd14d0
Kunj121 <shahkm2001@gmail.com>
2025-11-29 14:53:36 -0600

    trained_model

--- settings.py
+++ settings.py
@@ -9,5 +9,5 @@ TRAINING_PLOT_DIR = Path(__file__).resolve().parent / 'plots'/ 'training_plots'
 # file = DATA_DIR / "0056_md_202310_202310.csv.gz"
 
 # params (for display + filename)
-G_PARAMS = {'lr': 0.0004, 'betas': (0.99, 0.999)}
-D_PARAMS = {'lr': 0.00015,   'betas': (0.99, 0.999)}
+G_PARAMS = {'lr': 0.00375, 'betas': (0.99, 0.999)}
+D_PARAMS = {'lr': 0.00115,   'betas': (0.99, 0.999)}
